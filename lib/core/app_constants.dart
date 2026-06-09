9d1ee592f4ef841aaa87de76673215ed23b0ad05
FazaHumairah <fazahumairah03@gmail.com>
2026-06-10 01:52:44 +0700

    Update API base URL to server v10 (port 8000)

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   AppConstants._();
 
-  static const String apiBaseUrl   = 'http://192.168.88.134:8000';
+  static const String apiBaseUrl   = 'http://192.168.161.203:8000';
   static const String appName      = 'SnapSign';
   static const String appVersion   = '1.0.0';
 }
